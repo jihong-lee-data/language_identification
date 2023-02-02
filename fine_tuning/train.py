4f852dee5da8d69a7513035be8e46a1ccb119165
jihong.lee <jihong.lee@flitto.com>
2023-02-02 17:31:34 +0900

    not using gpu on tokenizer

--- fine_tuning/train.py
+++ fine_tuning/train.py
@@ -12,24 +12,23 @@ import os
 
 def main():
     device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cuda:1' if torch.cuda.is_available() else 'cpu')
-    print("Device: ", device)
+    print("device: ", device)
 
     # loading dataset
     print("Loading dataset...")
-    
+
     if os.path.exists("data/tokenized/wortschartz_30/"):
         tokenized_datasets = load_from_disk("data/tokenized/wortschartz_30/")
     else:
+        print("tokenizing dataset...")
         datasets = load_from_disk("../model_development/data/wortschartz_30/")
-        tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base").to(device)
-        print("tokenizer device: ", tokenizer.device)
+        tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
 
         def tokenize_function(examples):
             return tokenizer(examples['text'], padding="max_length", truncation= True)
 
         tokenized_datasets = datasets.map(tokenize_function, batched=True)
         tokenized_datasets.save_to_disk('data/tokenized/wortschartz_30')
-
 
     print("Done")
 
@@ -43,7 +42,7 @@ def main():
     
     # loading base model
     model = AutoModelForSequenceClassification.from_pretrained("xlm-roberta-base", num_labels=30).to(device)
-    print("model device: ", model.device)
+    
 
     default_args = {
         "output_dir": "test_trainer",
@@ -55,9 +54,12 @@ def main():
         "do_train": True,
         "do_eval": True,
         "evaluation_strategy": "steps",
-        "use_mps_device": True,
+        
         "seed": 42
     }
+    
+    if device == 'mps:0':
+        default_args['use_mps_device'] = True
 
     training_args = TrainingArguments(**default_args)
 
