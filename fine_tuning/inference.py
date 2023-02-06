68c77a83fe7fd034ebe93abc2df0b3ed49a997c6
jihong.lee <jihong.lee@flitto.com>
2023-02-06 16:52:30 +0900

    update: test checkpoint model

--- fine_tuning/inference.py
+++ fine_tuning/inference.py
@@ -2,18 +2,20 @@ import os
 import sys
 import pytorch_lightning as pl
 import torch.nn as nn
-from transformers import AutoTokenizer, RobertaModel, RobertaConfig
+from transformers import AutoTokenizer, RobertaForSequenceClassification, RobertaConfig
 import torch
 
 LABELS = ['ar', 'cs', 'da', 'de', 'el', 'en', 'es', 'fi', 'fr', 'he', 'hi', 'hu', 'id', 'it', 'ja', 'ko', 'ms', 'nl', 'pl', 'pt', 'ru', 'sv', 'sw', 'th', 'tl', 'tr', 'uk', 'vi', 'zh_cn', 'zh_tw']
 
-device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cpu')
+torch.cuda.empty_cache()
 
 label_dict = dict(zip(range(len(LABELS)), LABELS))
 
-LOCAL_PATH = "test_trainer/checkpoint-500"
+LOCAL_PATH = "test_trainer/checkpoint-57600"
 FILE_NAME = "pytorch_model.bin"
-LOCAL_DATA_PATH = os.path.join(LOCAL_PATH, FILE_NAME)
+LOCAL_W_PATH = os.path.join(LOCAL_PATH, FILE_NAME)
 
 # print(LOCAL_DATA_PATH)
 
@@ -26,7 +28,7 @@ class Classifier:
 
     def classify(self, text):
         probs, logits = self.model(text)
-        return probs[0].detach().cpu().numpy(), logits[0].detach().cpu().numpy()        
+        return probs[0], logits[0]
 
 
     def get_max_n(self, values: list, n = 3):
@@ -42,10 +44,13 @@ class Classifier:
 class load_model(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.roberta = RobertaModel.from_pretrained("xlm-roberta-base").to(device)
+        # self.roberta = RobertaModel(config = config).to(device)
+        self.model = RobertaForSequenceClassification(config).to(device)
+        self.model.load_state_dict(torch.load(LOCAL_W_PATH, map_location=device))
         self.tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-        self.classifier = nn.Linear(self.roberta.config.hidden_size, 30).to(device)
-        
+        # self.classifier = nn.Linear(self.roberta.config.hidden_size, 30).to(device)
+    
+    
     def forward(self, text:str):
         encoding = self.tokenizer.encode_plus(
             text,
@@ -57,13 +62,11 @@ class load_model(pl.LightningModule):
             return_attention_mask=True,
             return_tensors='pt',
         ).to(device)
-        output = self.roberta(encoding["input_ids"], attention_mask=encoding["attention_mask"])
-        output = output.last_hidden_state[:,0,:]
-        logits = self.classifier(output)
+        output = self.model(encoding["input_ids"], attention_mask=encoding["attention_mask"])
+        logits = output['logits']
         probs = torch.softmax(logits, -1)
         torch.cuda.empty_cache()
-        
-        return probs, logits
+        return probs.detach().cpu().numpy(), logits.detach().cpu().numpy()
 
 def main():
     clf = Classifier(label_dict)
@@ -73,9 +76,9 @@ def main():
             text= input('Enter text: ')
             probs, logits = clf.classify(text)
 
-            preds_3, probs_3 = clf.get_max_n(probs, n = 3)
+            preds_n, probs_n = clf.get_max_n(probs, n = 3)
             torch.cuda.empty_cache()
-            print(dict(zip(preds_3, probs_3)))
+            print(dict(zip(preds_n, probs_n)))
 
         except EOFError:
             print('Bye!')
