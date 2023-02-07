94a50a502e83686eaa4bde8b35a2d50384547b9f
jihong.lee <jihong.lee@flitto.com>
2023-02-07 07:15:14 +0000

    update: train.py

--- fine_tuning/train.py
+++ fine_tuning/train.py
@@ -6,6 +6,9 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
 import os
+import warnings
+warnings.filterwarnings(action='ignore')
+
 
 def main():
     device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,33 +32,46 @@ def main():
 
     print("Done")
 
+    train_size = len(tokenized_datasets["train"])
+    valid_size = len(tokenized_datasets["validation"])
+
+    print("Dataset size(train): ", train_size)
+    print("Dataset size(validation): ", valid_size)
+
     metric = evaluate.load("accuracy")
 
     def compute_metrics(eval_pred):
-        global metric
         logits, labels = eval_pred
         predictions = np.argmax(logits, axis=-1)
         return metric.compute(predictions=predictions, references=labels)
-    
+
     # loading base model
     model = AutoModelForSequenceClassification.from_pretrained("xlm-roberta-base", num_labels=30).to(device)
-    
+
 
     default_args = {
         "output_dir": "test_trainer",
         "num_train_epochs": 1,
         "log_level": "error",
         "per_device_train_batch_size":25,
-        "per_device_eval_batch_size":5,
+        "per_device_eval_batch_size":25,
+        "label_names": tokenized_datasets['train'].features['labels'].names,
         "logging_steps": 1000,
         "do_train": True,
         "do_eval": True,
         "evaluation_strategy": "steps",
-        "seed": 42
+        "dataloader_num_workers": 20,
+        "seed": 42,
     }
+
+    train_n_step = train_size / default_args["per_device_train_batch_size"]
+    valid_n_step = valid_size / default_args["per_device_eval_batch_size"]
+
+    default_args["save_steps"]= train_n_step // 5
     
     if device == 'mps':
-        default_args['use_mps_device'] = True
+        default_args['use_mps_device']= True
+        default_args["dataloader_num_workers"]= 20
 
     training_args = TrainingArguments(**default_args)
 
