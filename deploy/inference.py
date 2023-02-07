dcbec17a2005fdf9398d081b41d8285e0b410266
jihong.lee <jihong.lee@flitto.com>
2023-02-07 16:23:54 +0900

    update: model_qunatization

--- deploy/inference.py
+++ deploy/inference.py
@@ -1,11 +1,13 @@
 from module.engine import *
 from nltk.corpus import words
+import time
 
 model = Model("best_estimator")
 
 while True:
     try:
         text= input('text: ')
+        start = time.time()
         if not tokenizer(text):
             lang_pred_dict = {}
         elif text.strip() in words.words():
@@ -20,6 +22,8 @@ while True:
         if lang_pred_dict is not None:
             json_str = json.dumps(lang_pred_dict)
             print(json_str)
+        end = time.time()
+        print(f"Inference time: ({end - start:.5f} sec)", end = '\n'*2)
     except EOFError:
         print("End!")
         break
