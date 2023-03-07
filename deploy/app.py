69b8a39384f3ac30267c1fec28946bc2cf3c5e4c
jihong.lee <jihong.lee@flitto.com>
2023-03-07 12:32:33 +0900

    update: app.py

--- deploy/app.py
+++ deploy/app.py
@@ -15,27 +15,25 @@ class JSON(BaseModel):
     
 app= FastAPI()
 
-clf= Inference()
+app.model= Inference()
 
 nltk.download('wordnet')
 
-
+@app.get('/')
+async def root():
+    return 'Language Identification API'
+    
 @app.post('/api/langid')
 async def predict(request:JSON):
-    # request_json= await request.json()
     request_json= jsonable_encoder(request)
-    if request_json is None:
-        return HTTPException(400)
-    if 'text' not in request_json:
-        return HTTPException(400)        
+    
     text= request_json['text']
     if not rm_spcl_char(text):
         lang_pred_dict= {}
     elif text.strip() in wordnet.words():
         lang_pred_dict= {'en': 1.00}
     else:    
-        lang_pred_dict= clf.predict(text, n= 3)
- 
+        lang_pred_dict= app.model.predict(text, n= 3)
     if lang_pred_dict is not None:
         return jsonable_encoder(lang_pred_dict)
     
