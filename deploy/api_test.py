263e3e14306dd1939c5c7eecdfa142b6ba0f919f
jihong.lee <jihong.lee@flitto.com>
2023-03-08 12:10:08 +0900

    update

---
+++ deploy/api_test.py
@@ -0,0 +1,16 @@
+import json
+from locust import HttpUser, task, between
+import pandas as pd
+
+test_data = pd.read_excel("../../place_tm_20230216_053926.xlsx")
+
+
+class PerformanceTests(HttpUser):
+    # wait_time = between(1, 3)
+
+    @task(1)
+    def testFastApi(self):
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        text = {"text": test_data.sample(1)['dst_content'].values[0]}
+        data = json.dumps(text)
+        self.client.post("/api/langid", data=data, headers=headers)
