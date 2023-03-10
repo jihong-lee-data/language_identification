575541016119fb041b34ca60f03d23b7619a0d3d
jihong.lee <jihong.lee@flitto.com>
2023-03-10 09:02:08 +0000

    update: modify service

--- deploy/app/main.py
+++ deploy/app/main.py
@@ -1,23 +1,37 @@
-from app.inference import Inference
-from app.tool import rm_spcl_char
+import numpy as np
 from fastapi import FastAPI
+from typing import List, Union
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 import nltk
 from nltk.corpus import wordnet
-from pydantic import BaseModel
-from typing import Union, List
-from fastapi.encoders import jsonable_encoder
-import numpy as np
+from app.inference import Inference
+from app.tool import rm_spcl_char, ISO
 
-class Request(BaseModel):
-    text: Union[str, List[str]]
-    n: int= 1
-    
-
-app= FastAPI()
-
-app.model= Inference(device='cpu')
-
+app = FastAPI()
+app.model = Inference(device='cpu')
+app.iso = ISO()
 nltk.download('wordnet')
+
+
+class LangIdRequest(BaseModel):
+    text: str
+    n: int= 3
+
+# class LangIdRequest(BaseModel):
+#     text: Union[str, List[str]]
+#     n: int = 1
+
+class ISOSearchRequest(BaseModel):
+    query: str
+    tol: int = 2
+    code: str = ''
+
+
+class ISOConvertRequest(BaseModel):
+    src: str
+    src_code: str = ''
+    dst_code: str = '1'
 
 
 @app.get('/')
@@ -26,24 +40,71 @@ async def root():
 
 
 @app.post('/api/langid')
-async def predict(request: Request):
+async def predict(request: LangIdRequest):
+    request_json = jsonable_encoder(request)
+    text = request_json['text']
+    n = request_json['n']
+    
+    
+    text_prepped= rm_spcl_char(text)
+    
+    lang_pred = {} if not text_prepped else ({'en': 1.00} if text_prepped.strip() in wordnet.words() else None)
+    
+    if not isinstance(lang_pred, dict):
+        lang_pred= app.model.predict(text_prepped, n= n)
+
+    return jsonable_encoder(lang_pred) 
+    
+# ToDo - batch inference & new response formatting
+@app.post('/api/langidb')
+async def predict_b(request: LangIdRequest):
     request_json = jsonable_encoder(request)
     text = request_json['text']
     n = np.clip(request_json['n'], a_min=1, a_max=len(app.model.label_dict))
+    dst_code = request_json['type']
 
     if isinstance(text, str):
         text = [text]
-    
+
+    if len(text) > 1000:
+        return jsonable_encoder(dict(result=[]))
+
     lang_pred = np.array([
-                          {} if not rm_spcl_char(tmp_text) else ({'en': 1.00} if tmp_text.strip() in wordnet.words() else None)
-                           for tmp_text in text
-                        ])
-    
+        {} if not rm_spcl_char(tmp_text) else ({'en': 1.00} if tmp_text.strip() in wordnet.words() else None)
+        for tmp_text in text
+    ])
+
     text_for_model = np.array(text)[lang_pred == None].tolist()
 
     if text_for_model:
         model_pred = app.model.predict(text=text_for_model, n=n)
         lang_pred[lang_pred == None] = model_pred
 
-    result = dict(result=lang_pred.tolist())
-    return jsonable_encoder(result)
+    result = dict(result=lang_pred.tolist()) if len(text) > 1 else lang_pred[0]
+
+    if dst_code:
+        dst_code = dst_code[0]
+
+
+@app.post('/api/iso/search')
+async def search(request: ISOSearchRequest):
+    request_json = jsonable_encoder(request)
+    query = request_json['query']
+    code = request_json['code']
+    tol = request_json['tol']
+
+    return jsonable_encoder(dict(result=app.iso(query=query, code=code, tol=tol)))
+
+
+@app.post('/api/iso/convert')
+async def convert(request: ISOConvertRequest):
+    request_json = jsonable_encoder(request)
+    src = request_json['src']
+    src_code = request_json['src_code']
+    dst_code = request_json['dst_code']
+    try:
+        result= jsonable_encoder(dict(result=app.iso.convert(src=src, src_code=src_code, dst_code=dst_code)))
+    except ValueError: 
+        result= jsonable_encoder(dict(result=[]))
+
+    return result
--- deploy/app/tool.py
+++ deploy/app/tool.py
@@ -1,6 +1,7 @@
 import json
+import os
 import re
-import numpy as np
+from pathlib import Path
 
 def load_json(path): 
     return json.load(open(path, "r"))
@@ -8,10 +9,121 @@ def load_json(path):
 def save_json(file, path): 
     json.dump(file, open(path, "w"))
 
+def mk_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    
 def rm_spcl_char(text):
     # remove special characters
     text = re.sub(r'[!@#$(),，\n"%^*?？:;~`0-9&\[\]\。\/\.\=\-]', ' ', text)
     text = re.sub(r'[\s]{2,}', ' ', text.strip())
     text = text.lower().strip()
-    
     return text
+
+class ISO:
+    """
+    ISO Language Code Search and Conversion Tool
+
+    This class provides a tool for searching and converting language codes from ISO 639-1 and 639-2 standards.
+    It uses a JSON file of language codes that is downloaded from a GitHub repository if it is not found locally.
+
+    """
+    def __init__(self):
+        """
+        Initializes the ISO class by downloading the language code dictionary if it is not found locally,
+        loading the JSON file into a dictionary, and creating a dictionary to map letters to language attributes.
+        """
+        self.dict_path = Path('resource/iso.json')
+        if not self.dict_path.exists():
+            self.dict_path.parent.mkdir(parents=True, exist_ok=True)
+            print(f"Downloading 'ISO dictionary'..")
+            os.system(f"curl <https://raw.githubusercontent.com/jihong-lee-data/gadgetbox/main/{str(self.dict_path)}> > {str(self.dict_path)}")
+            print(f"{'---'*10}\\n'ISO dictionary' is saved to {str(self.dict_path)}")
+        self.iso_dict = load_json(str(self.dict_path))
+        self.code_dict = dict(zip(['e', 'k', '1', '2'], ['English', 'Korean', 'ISO 639-1 Code', 'ISO 639-2 Code']))
+
+    def __call__(self, query, code=None, tol=2):
+        """
+        Searches the dictionary for entries matching the query and returns the results.
+
+        Args:
+        - query: string - The search query to match against the dictionary
+        - code: string - A letter code representing an attribute of the language (e.g. 'e' for English)
+        - tol: int - The tolerance level for matching. 0 for exact matches, 1 for case-insensitive, 2 for partial matches
+
+        Returns:
+        - list - A list of dictionary entries that match the search query and code (if specified)
+        """
+        return self._search(query, code, tol)
+
+    def _search(self, query, code=None, tol=2):
+        """
+        Helper function to search the dictionary for matching entries.
+
+        Args:
+        - query: string - The search query to match against the dictionary
+        - code: string - A letter code specifying the target cateogry  (e.g., 'e' for English, 'k' for Korean, '1' for ISO 639-1, and '2' for ISO 639-2)
+        - tol: int - The tolerance level for matching. 0 for exact matches, 1 for case-insensitive, 2 for partial matches
+
+        Returns:
+        - list - A list of dictionary entries that match the search query and code (if specified)
+        """
+        results = []
+        for row in self.iso_dict:
+            if code:
+                target = '%'.join([row.get(i) for i in row if i in [self.code_dict.get(c) for c in list(code)]])
+            else:
+                target = '%'.join(row.values())
+            if self._word_validation(query, target, tol):
+                results.append(row)
+        return results
+
+    def convert(self, src, src_code=None, dst_code=None):
+        """
+        Converts a language code from one standard to another.
+
+        Args:
+        - src: string - A source to convert
+        - src_code(Optional): string - A letter code representing an attribute of the source (e.g. 'e' for English)
+        - dst_code: string - A letter code representing the destination language to which the source convert (e.g. '1' for ISO 639-1)
+
+        Returns:
+        - string or list - A string or list of strings representing the converted language code(s)
+        """
+        search_result = self._search(src, code=src_code, tol=1)
+        if not search_result:
+            raise ValueError('An input query can be case-insensitive but must be matched to a target completely.')
+        else:
+            result = search_result.pop(0)
+
+        if dst_code:
+            if len(dst_code) > 1:
+                return [result.get(self.code_dict.get(d)) for d in list(dst_code)]
+            else:
+                return result.get(self.code_dict.get(dst_code))
+        else:
+            return result.values()
+
+    def _word_validation(self, test:str, target:str, tol=2):
+        """
+        Helper function to validate matches based on the tolerance level.
+
+        Args:
+        - test: string - The search query to match against the target string
+        - target: string - The string to match against the search query
+        - tol: int - The tolerance level for matching. 0 for exact matches, 1 for case-insensitive, 2 for partial matches
+
+        Returns:
+        - bool - True if the search query matches the target string based on the tolerance level, False otherwise
+        """
+        if tol == 0: # exact matches
+            target_list = target.split('%')
+            return test in target_list
+        elif tol == 1: # case-insensitive
+            target_list = target.lower().split('%')
+            return test.lower() in target_list
+        elif tol == 2: # partial matches
+            return test.lower() in target.lower()
+            
+        
