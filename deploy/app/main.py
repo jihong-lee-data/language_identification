48241759de7c3932251a97f07e9d7c8493f027a0
jihong.lee <jihong.lee@flitto.com>
2023-03-14 12:49:09 +0900

    update: add exceptions

--- deploy/app/main.py
+++ deploy/app/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import List, Union, Dict, Any
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 import nltk
@@ -50,12 +50,16 @@ class ISOConvertResponse(BaseModel):
 
 @app.get("/")
 async def root():
+    """
+    The home page of the API.
+    """
     return "Language Identification API"
 
 
 @app.post("/api/langid/predict")
 async def predict(request: LangIdRequest):
-    """Predicts the language of a given text.
+    """
+    Predicts the language of a given text.
 
     Args:
         request (LangIdRequest): The request object containing the text to be predicted and other parameters.
@@ -66,17 +70,21 @@ async def predict(request: LangIdRequest):
     request_json = jsonable_encoder(request)
     text = request_json["text"]
     n = request_json["n"]
-
-    text_prepped = remove_character(text, target="sde")
-
-    lang_pred = (
-        {}
-        if not text_prepped
-        else ({"en": 1.00} if text_prepped.strip() in wordnet.words() else None)
-    )
-
-    if not isinstance(lang_pred, dict):
-        lang_pred = app.model.predict(text_prepped, n=n).pop(0)
+    try:
+        text_prepped = remove_character(text, target="sde")
+        lang_pred = (
+            {}
+            if not text_prepped
+            else (
+                {"en": 1.00}
+                if text_prepped.strip() in wordnet.words()
+                else None
+            )
+        )
+        if not isinstance(lang_pred, dict):
+            lang_pred = app.model.predict(text_prepped, n=n).pop(0)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     return jsonable_encoder(lang_pred)
 
@@ -91,72 +99,78 @@ async def predict_batch(request: LangIdBatchRequest):
     Returns:
         JSON: A JSON object containing the predicted languages and their probabilities.
     """
-    request_json = jsonable_encoder(request)
-    text = request_json["text"]
-    n = np.clip(request_json["n"], a_min=1, a_max=len(app.model.label_dict))
+    try:
+        request_json = jsonable_encoder(request)
+        text = request_json["text"]
+        n = np.clip(request_json["n"], a_min=1, a_max=len(app.model.label_dict))
 
-    if isinstance(text, str):
-        text = [text]
+        if isinstance(text, str):
+            text = [text]
 
-    if len(text) > 1000:
-        return jsonable_encoder({"result": []})
+        if len(text) > 1000:
+            return jsonable_encoder({"result": []})
 
-    text_prepped = [remove_character(sample, target="sde") for sample in text]
-
-    lang_pred = np.array(
-        [
-            [{}]
-            if not tmp_text
-            else [
-                {
-                    "predicion": "en",
-                    "probability": 1.00,
-                    "rank": 1,
-                    "english": "English",
-                    "korean": "영어",
-                }
-            ]
-            if tmp_text.strip() in wordnet.words()
-            else [None]
-            for tmp_text in text_prepped
+        text_prepped = [
+            remove_character(sample, target="sde") for sample in text
         ]
-    )
 
-    text_for_model = (
-        np.array(text)[(lang_pred == None).squeeze()].squeeze().tolist()
-    )
-
-    if text_for_model:
-        model_pred = app.model.predict(text=text_for_model, n=n)
-
-        model_result = np.array(
+        lang_pred = np.array(
             [
-                [
+                [{}]
+                if not tmp_text
+                else [
                     {
-                        "prediction": pred,
-                        "probability": prob,
-                        "rank": idx + 1,
-                        "english": app.iso.convert(
-                            src=pred, src_code="1", dst_code="e"
-                        ),
-                        "korean": app.iso.convert(
-                            src=pred, src_code="1", dst_code="k"
-                        ),
+                        "predicion": "en",
+                        "probability": 1.00,
+                        "rank": 1,
+                        "english": "English",
+                        "korean": "영어",
                     }
-                    for idx, (pred, prob) in enumerate(row.items())
                 ]
-                for row in model_pred
+                if tmp_text.strip() in wordnet.words()
+                else [None]
+                for tmp_text in text_prepped
             ]
-        ).tolist()
+        )
 
-        result = {
-            "result": [
-                model_result.pop(0) if row == [None] else row.tolist()
-                for row in lang_pred
-            ]
-        }
-    else:
-        result = {"result": lang_pred.tolist()}
+        text_for_model = (
+            np.array(text)[(lang_pred == None).squeeze()].squeeze().tolist()
+        )
+
+        if text_for_model:
+            model_pred = app.model.predict(text=text_for_model, n=n)
+
+            model_result = np.array(
+                [
+                    [
+                        {
+                            "prediction": pred,
+                            "probability": prob,
+                            "rank": idx + 1,
+                            "english": app.iso.convert(
+                                src=pred, src_code="1", dst_code="e"
+                            ),
+                            "korean": app.iso.convert(
+                                src=pred, src_code="1", dst_code="k"
+                            ),
+                        }
+                        for idx, (pred, prob) in enumerate(row.items())
+                    ]
+                    for row in model_pred
+                ]
+            ).tolist()
+
+            result = {
+                "result": [
+                    model_result.pop(0) if row == [None] else row.tolist()
+                    for row in lang_pred
+                ]
+            }
+        else:
+            result = {"result": lang_pred.tolist()}
+
+    except Exception as e:
+        result = {"error": str(e)}
 
     return jsonable_encoder(result)
 
@@ -169,16 +183,17 @@ async def search(request: ISOSearchRequest):
         request (ISOSearchRequest): The request object containing the query, ISO code table, and tolerance level.
 
     Returns:
-        JSON: A JSON object containing the search result.
+        JSON: A JSON object containing the search result or an error message.
     """
-    request_json = jsonable_encoder(request)
-    query = request_json["query"]
-    code = request_json["code"]
-    tol = request_json["tol"]
-
-    return jsonable_encoder(
-        {"result": app.iso(query=query, code=code, tol=tol)}
-    )
+    try:
+        request_json = jsonable_encoder(request)
+        query = request_json["query"]
+        code = request_json["code"]
+        tol = request_json["tol"]
+        result = app.iso(query=query, code=code, tol=tol)
+        return jsonable_encoder({"result": result})
+    except Exception as e:
+        return jsonable_encoder({"error": str(e)})
 
 
 @app.post("/api/iso/convert")
